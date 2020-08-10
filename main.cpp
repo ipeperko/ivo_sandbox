61b558fc2af944881e729fcc5b82cb6e03b73da3
ivo <ivo.peperko@i13tech.com>
2020-08-10 10:12:53 +0200

    Mysql lib

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "mysql/mysql.h"
 
 int main()
 {
+    MYSQL* mysql = mysql_init(nullptr);
+    mysql_close(mysql);
+
 #ifdef TEST_OPTION
     std::cout << "[ok] Test option enabled\n";
     return 0;
